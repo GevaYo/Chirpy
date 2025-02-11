03c912ef4223766226d51d09bfb6833517e90d08
GevaYo <gevayo@mta.ac.il>
2025-02-11 14:45:13 +0200

    created '/healthz' end point

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GevaYo/Chirpy
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthHandler)
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	fmt.Println("Listening on port 8080...")
+	log.Fatal(server.ListenAndServe())
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
